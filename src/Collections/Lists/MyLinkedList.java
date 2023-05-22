795ea17e6025f0e2c6fb7dbe54a577c75a48354c
sergey <skarapetean@gmail.com>
2023-05-22 20:13:05 +0400

    "third commit"

--- src/Collections/Lists/MyLinkedList.java
+++ src/Collections/Lists/MyLinkedList.java
@@ -1,6 +1,7 @@
 package Collections.Lists;
 import Collections.MyCollection;
 import Collections.Queues.MyDeque;
+import java.util.LinkedList;
 
 import java.util.Iterator;
 import java.util.ListIterator;
@@ -12,9 +13,9 @@ public class MyLinkedList<E> implements MyDeque<E>, MyList<E>{
     private Node<E> first;
     private Node<E> last;
 
-    MyLinkedList() {}
+    public MyLinkedList() {}
 
-    MyLinkedList(MyCollection<? extends E> col) {
+    public MyLinkedList(MyCollection<? extends E> col) {
         this();
         this.addAll(col);
     }
@@ -54,19 +55,37 @@ public class MyLinkedList<E> implements MyDeque<E>, MyList<E>{
             this.first = newNode;
         } else {
             tmp.next = newNode;
+
         }
 
         ++this.size;
     }
 
+    private void linkBefore(E el, Node<E> b) {
+        Node<E> tmp = this.first;
+
+        while ( tmp != this.last) {
+            if (tmp == b) {
+                tmp.prev = new Node<>(el, tmp, tmp.prev);
+                tmp.prev.prev.next = tmp.prev;
+                break;
+            }
+            tmp = tmp.next;
+        }
+
+        ++this.size;
+    }
     private E unlink(Node<E> node) {
         E data = node.item;
         Node<E> next = node.next;
         Node<E> prev = node.prev;
+
         if (prev == null) {
             this.first = next;
+            this.first.prev = null;
         } else if (next == null) {
             this.last = prev;
+            this.last.next = null;
         } else {
             next.prev = prev;
             prev.next = next;
@@ -74,6 +93,18 @@ public class MyLinkedList<E> implements MyDeque<E>, MyList<E>{
 
         --this.size;
         return data;
+    }
+
+    private void checkElementIndex(int index) {
+        if (index < 0 || index >= this.size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private void checkPositionIndex(int index){
+        if (index < 0 || index > this.size) {
+            throw new IndexOutOfBoundsException();
+        }
     }
     @Override
     public int size() {
@@ -87,7 +118,7 @@ public class MyLinkedList<E> implements MyDeque<E>, MyList<E>{
 
     @Override
     public boolean contains(Object var1) {
-        return false;
+        return this.indexOf(var1) >= 0;
     }
 
     @Override
@@ -97,67 +128,196 @@ public class MyLinkedList<E> implements MyDeque<E>, MyList<E>{
 
     @Override
     public boolean remove(Object var1) {
+        Node<E> tmp;
+        if (var1 == null) {
+            for (tmp = this.first; tmp != null; tmp = tmp.next) {
+                if (tmp.item == null) {
+                    this.unlink(tmp);
+                    return true;
+                }
+            }
+        } else {
+            for (tmp = this.first; tmp != null; tmp = tmp.next) {
+                if (tmp.item.equals(var1)) {
+                    this.unlink(tmp);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public boolean addAll(MyCollection<? extends E> vars1) {
-        return false;
+        Iterator<?> itr = vars1.iterator();
+        while (itr.hasNext()) {
+            this.add((E) itr.next());
+        }
+        return true;
     }
 
     @Override
     public boolean addAll(int index, MyCollection<? extends E> vars2) {
-        return false;
+        this.checkPositionIndex(index);
+        Object[] arr = vars2.toArray();
+
+        for (int i = 0; i < arr.length; ++i) {
+            this.add(index + i, (E)arr[i]);
+        }
+
+        return arr.length > 0;
     }
 
     @Override
     public boolean removeAll(MyCollection<?> vars1) {
-        return false;
+        Iterator<?> itr = vars1.iterator();
+        while (itr.hasNext()) {
+            this.remove(itr.next());
+        }
+        return true;
     }
 
     @Override
     public boolean containsAll(MyCollection<?> vars1) {
-        return false;
+        Iterator<?> itr = vars1.iterator();
+        boolean bool = true;
+
+        while (itr.hasNext()) {
+            if (!this.contains(itr.next())) {
+                bool = false;
+                break;
+            }
+        }
+
+        return bool;
     }
 
     @Override
-    public boolean retainAll(MyCollection<?> vars1) {
-        return false;
+    public boolean retainAll(MyCollection<?> col) {
+        if (col == null) {
+            throw new NullPointerException();
+        }
+
+        boolean modified = false;
+        Node<E> tmp = this.first;
+
+        while (tmp != null) {
+            if (!col.contains(tmp.item)) {
+                System.out.println(this.unlink(tmp));
+                modified = true;
+            }
+            tmp = tmp.next;
+        }
+
+        return modified;
     }
 
     @Override
     public void clear() {
+        Node<E> tmp;
+        Node<E> next;
 
+        for (tmp = this.first; tmp != null; tmp = next) {
+            next = tmp.next;
+            tmp.item = null;
+            tmp.prev = null;
+            tmp.next = null;
+        }
+
+        this.size = 0;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        this.checkElementIndex(index);
+        return this.getNode(index).item;
     }
 
     @Override
     public E set(int index, E var1) {
-        return null;
+        this.checkPositionIndex(index);
+        Node<E> tmp = this.getNode(index);
+        E el = tmp.item;
+        tmp.item = var1;
+        return el;
     }
 
     @Override
     public void add(int index, E var1) {
-
+        this.checkPositionIndex(index);
+        this.linkBefore(var1, this.getNode(index));
     }
 
+    private Node<E> getNode(int index) {
+        Node<E> tmp;
+
+        if (index > this.size >> 1) {
+            tmp = this.last;
+            for (int i = this.size - 1; i > index; --i) {
+                tmp = tmp.prev;
+            }
+        } else {
+            tmp = this.first;
+            for (int i = 0; i < index; ++i) {
+                tmp = tmp.next;
+            }
+        }
+
+        return tmp;
+    }
     @Override
     public E remove(int index) {
+        this.checkElementIndex(index);
+        this.unlink(this.getNode(index));
         return null;
     }
 
     @Override
     public int indexOf(Object var1) {
-        return 0;
+        int index = 0;
+        Node<E> tmp;
+
+        if (var1 == null) {
+            for (tmp = this.first; tmp != null; tmp = tmp.next) {
+                if (tmp.item == null) {
+                    return index;
+                }
+                ++index;
+            }
+        } else {
+            for (tmp = this.first; tmp != null; tmp = tmp.next) {
+                if (var1.equals(tmp.item)) {
+                    return index;
+                }
+                ++index;
+            }
+        }
+
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Object var1) {
-        return 0;
+        int index = this.size - 1;
+        Node<E> tmp;
+
+        if (var1 == null) {
+            for (tmp = this.last; tmp != null; tmp = tmp.prev) {
+                if (tmp.item == null) {
+                    return index;
+                }
+                --index;
+            }
+        } else {
+            for (tmp = this.last; tmp != null; tmp = tmp.prev) {
+                if (tmp.item.equals(var1)) {
+                    return index;
+                }
+                --index;
+            }
+        }
+
+        return -1;
     }
 
     @Override
@@ -177,7 +337,14 @@ public class MyLinkedList<E> implements MyDeque<E>, MyList<E>{
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] result = new Object[this.size];
+        int index = 0;
+
+        for (Node<E> tmp = this.first; tmp != null; tmp = tmp.next) {
+            result[index++] = tmp.item;
+        }
+
+        return result;
     }
 
     @Override
